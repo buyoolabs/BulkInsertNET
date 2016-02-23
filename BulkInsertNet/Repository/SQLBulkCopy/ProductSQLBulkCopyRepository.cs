160288b036f3e1efa8d06e0218bfe1a412d72eee
Jorge Sanchez <xurxosf@gmail.com>
2016-02-23 10:20:18 +0100

    add Product and Category repository structure

---
+++ BulkInsertNet/Repository/SQLBulkCopy/ProductSQLBulkCopyRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkInsertNet.Repository.SQLBulkCopy
+{
+    public class ProductSQLBulkCopyRepository:IProductRepository
+    {
+    }
+}

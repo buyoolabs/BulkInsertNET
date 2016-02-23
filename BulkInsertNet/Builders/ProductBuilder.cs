63db6033b678c4e0d93a682a09687867299571ca
mbuenoferrer <mbuenoferrer@gmail.com>
2016-02-23 12:39:16 +0100

    Added missing files

---
+++ BulkInsertNet/Builders/ProductBuilder.cs
@@ -0,0 +1,21 @@
+﻿using BulkInsertNET.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkInsertNet.Builders
+{
+    public static class ProductBuilder
+    {
+        public static IEnumerable<Product> BuildCollection(int numberOfItems, IEnumerable<Category> categories)
+        {
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                var product = new Product() { Title = string.Format("My Product {0}", i), Price = 2.68m * i, Stock = i + 8, IsActive = true, Brand = string.Format("My Brand {0}", i), Categories = categories };
+                yield return product;
+            }
+        }
+    }
+}
---
+++ BulkInsertNet/Database/Helper.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkInsertNet.Database
+{
+    public static class Helper
+    {
+
+        public static void DeleteAllData(string connectionString)
+        {
+            using (SqlConnection cnn = new SqlConnection(connectionString))
+            {
+                cnn.Open();
+                cnn.Execute(@"DELETE ProductCategories");
+                cnn.Execute(@"DELETE Categories");
+                cnn.Execute(@"DELETE Products");
+                cnn.Close();
+            }
+        }
+    }
+}
